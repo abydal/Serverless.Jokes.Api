6930f5fc5f9af947081829982dcad3060b5ca696
Asbjørn Bydal <asbjorn.bydal@if.no>
2019-05-09 20:23:40 +0200

    Add limiting and exclusion by categories

--- JokeClient.cs
+++ JokeClient.cs
@@ -5,9 +5,15 @@ namespace Jokes.Api {
     public static class JokeClient {
         static HttpClient client = new HttpClient ();
 
-        public static async Task<JokeResponse> GetRandomJoke () {
+        public static async Task<JokeResponse> GetRandomJoke (string[] limitTo, string[] exclude) {
 
-            var response = await client.GetAsync ("http://api.icndb.com/jokes/random");
+            var excludes = exclude.Length > 0 ? $"exlude=[{string.Join(",",exclude)}]" : string.Empty;
+            var limitedTo = limitTo.Length > 0 ? $"limitTo=[{string.Join (",", limitTo)}]" : string.Empty;
+            var queryString = string.Join ("&", limitedTo, excludes);
+            queryString = !string.IsNullOrEmpty (queryString) ? $"?{queryString}" : string.Empty;
+
+            var response = await client.GetAsync ($"http://api.icndb.com/jokes/random{queryString}");
+
             return Newtonsoft.Json.JsonConvert.DeserializeObject<JokeResponse> (await response.Content.ReadAsStringAsync ());
         }
     }
